1159db324371d73a853f3b11914748405a104c68
Hien Nguyen <hiennguyen92@gmail.com>
2024-01-10 15:06:58 +0700

    feat: update

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const express = require("express");
 const { ExpressPeerServer } = require("peer");
 
 const app = express();
-const router = express.Router();
 
 const port = process.env.PORT || 9000;
 
 app.get("/", (req, res, next) => res.send("Netlify Hosting Server By https://github.com/hiennguyen92"));
-
-router.get("/hello", (req, res) => res.send("Hello World!"));
 
 const server = app.listen(port);
 
@@ -16,5 +13,4 @@ const peerServer = ExpressPeerServer(server, {
 	path: "/server",
 });
 console.log("Server Started");
-app.use("/api/", router);
 app.use("/peerjs", peerServer);
